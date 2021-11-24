bb9c04739a7590d720bf77b45a4c0c103b6b8843
biatlon84 <biatlon84@gmail.com>
2021-11-25 02:55:20 +0300

    modified

--- Fibonacci/build/Release/.deps/Release/fib.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/fib.node := ln -f "Release/obj.target/fib.node" "Release/fib.node" 2>/dev/null || (rm -rf "Release/fib.node" && cp -af "Release/obj.target/fib.node" "Release/fib.node")
--- Fibonacci/build/Release/.deps/Release/obj.target/fib.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/obj.target/fib.node := g++ -o Release/obj.target/fib.node -shared -pthread -rdynamic -m64  -Wl,-soname=fib.node -Wl,--start-group Release/obj.target/fib/fib.o -Wl,--end-group 
